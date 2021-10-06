d4b6e10341ffa3c362f9028b4eab5b47766e2199
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-10-07 01:36:23 +0330

    ready

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const insertEl = document.getElementById('insert');
 
+window.addEventListener('keydown', (event) => {
+  insertEl.innerHTML = `
+    <div class="key">
+    ${event.key === ' ' ? 'Space' : event.key} 
+    <small>event.key</small>
+  </div>
+  
+  <div class="key">
+    ${event.keyCode}
+    <small>event.keyCode</small>
+  </div>
+  
+  <div class="key">
+    ${event.code}
+    <small>event.code</small>
+  </div>
+    `;
+});
